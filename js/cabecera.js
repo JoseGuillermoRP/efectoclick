8e8f9d50f8b87f0b687423c0af8705e7db94dd4e
Jose Rojas Paredes <jrojasp1992@gmail.com>
2020-03-28 19:30:52 -0500

    menu-dinamico

---
+++ js/cabecera.js
@@ -0,0 +1,22 @@
+Vue.component('cabecera', {
+    template: //html
+    `
+    <header class="">
+    <div class="contenedor-menu">
+   
+            <h1>EfectoClick</h1>
+        <div class="icon-menu" id="icon-menu"></div>
+        <nav class="nav" id="menu">
+            <a href="index.html">Inicio</a>
+            <a href="web.html">Desarrollo Web</a>
+            <a href="">Desarrollo de App</a>
+            <a href="">Producción AudioVisual</a>
+            <a href="">Marketing</a>
+            <a href="">Publicidad</a>
+        
+          
+    </div>
+   
+</header>
+    `
+});
